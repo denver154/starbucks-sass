00ca6d9cd8be6d89cd9cb5cfc751bcbd16db71cd
denver154 <loraceli20@gmail.com>
2023-05-17 22:43:19 -0700

    Starbucks 1st Commit

---
+++ dist/js/script.js
@@ -0,0 +1,42 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const headNav = document.querySelector(".header__nav");
+const header = document.querySelector(".header");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headNav.classList.toggle("open");
+  header.classList.toggle("open");
+  
+});
+
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((baba) => {
+  baba.addEventListener("click", () => {
+    baba.nextElementSibling.classList.toggle("open");
+    baba.querySelector("i").classList.toggle("open");
+  });
+});
+
+/*COFFEE TAB*/
+const starNav = document.querySelectorAll(".star__nav__link");
+const starContent = document.querySelectorAll(".coffee__tab__content");
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent = document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveStar() {
+  starNav.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  starContent.forEach((star) => {
+    star.classList.remove("active");
+  });
+}
